ff1779db7e5d955656922d072bd20df37e0c146a
Sofyan Ahalli <sofyan.ahalli@student.rocva.nl>
2020-06-04 04:55:53 +0200

    taak 10

---
+++ 2-Variabelen-en-datatype/taak10/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     
+     $item = "item= 1";
+     $item2 = "item= 4";
+     $item3 = "item= 7";
+
+     
+    ?>
+    <ul>
+    <li> $item = 1 </li>
+    <li> $item2 = 4 </li>
+    <li> $item3 = 7 </li>
+    </ul>
+    
+
+
+    </ul>
+</body>
+</html>
+ 
