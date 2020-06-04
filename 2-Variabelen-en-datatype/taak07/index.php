760ed807e09894e974202dcb0b99fc9f3fdb8c50
Sofyan Ahalli <sofyan.ahalli@student.rocva.nl>
2020-06-04 04:29:40 +0200

    taak 7

---
+++ 2-Variabelen-en-datatype/taak07/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    $leeftijd = "mijn naam is sofyan";
+    $voornaam = ", en ik ben18 jaar oud";
+    echo $leeftijd;
+
+    echo $voornaam;
+    ?>
+</body>
+</html>
+ 
