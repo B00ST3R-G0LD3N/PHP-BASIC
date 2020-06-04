dcfe99b7df3348b3def516d0696231ccc5503595
Sofyan Ahalli <sofyan.ahalli@student.rocva.nl>
2020-06-04 04:38:55 +0200

    taak 8

---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    $voornaam1 = "sofyan";
+    $voornaam2 = ", tarik";
+    $voornaam3 = ", dardan";
+
+    echo $voornaam1;
+    echo $voornaam2;
+    echo $voornaam3;
+    ?>
+</body>
+</html>
+ 
