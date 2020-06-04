a41c1a5107167987225a792ba03d014047d6f706
Sofyan Ahalli <sofyan.ahalli@student.rocva.nl>
2020-06-04 14:54:54 +0200

    taak 6

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $string = "twaalf";
+    $integer = 0;
+    $float = 0.1;
+    $boolean = true;
+    $null = null;
+
+    echo gettype($string);
+    echo gettype($integer);
+    echo gettype($float);
+    echo gettype($boolean);
+    echo gettype($null);
+
+
+
+    ?>
+</body>
+</html>
---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <?php
+    $myMovies = array("Spiderman", "Batman", "The Joker");
+echo "I like " . $myMovies[0] . ", " . $myMovies[1] . " and " . $myMovies[2] . ".";
+    ?>
+</body>
+</html>
+ 
