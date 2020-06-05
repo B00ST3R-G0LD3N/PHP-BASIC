096830e669d59b1c3c0a289512970e37a7e47bf9
Sofyan Ahalli <sofyan.ahalli@student.rocva.nl>
2020-06-05 04:13:41 +0200

    5 taak 1

---
+++ 5-Conditional_Statements/taak01/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $test = 12;
+    $test2 = "gewoon tekst";
+
+    $test != $test2;
+
+    var_dump($test , $test2);
+
+    ?>
+</body>
+</html>
