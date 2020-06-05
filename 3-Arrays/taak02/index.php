c70ce27353d61b2bf5ea8946526582a8de6b40b7
Sofyan Ahalli <sofyan.ahalli@student.rocva.nl>
2020-06-05 03:38:01 +0200

    taak 2 arays

---
+++ 3-Arrays/taak02/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $myDogNames[0] = 'kees';
+    $myDogNames[1] = 'jaap';
+    $myDogNames[2] = 'chris';
+
+    
+
+    ?>
+</body>
+</html>
