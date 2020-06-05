853d169c3e88d69520bc905aff2a2d38dfdab958
Sofyan Ahalli <sofyan.ahalli@student.rocva.nl>
2020-06-05 04:51:03 +0200

    taak 1

---
+++ 6-Functions/taak01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    zetTweeWoordenAanElkaar($helezin);
+    $woordje = "hallo";
+    $zinnetje = "mooie blauwe planeet";
+    $helezin ;
+    echo $helezin;
+
+    ?>
+</body>
+</html>
