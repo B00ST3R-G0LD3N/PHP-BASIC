05eeaa9d317d1f61bc0245b154a4e5e72c5bc647
Sofyan Ahalli <sofyan.ahalli@student.rocva.nl>
2020-06-05 05:03:21 +0200

    7 taak 1

---
+++ opdracht/includes/menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+    <?php
+    $menu = "menu-item1";
+    $menu2 = "menu-item2";
+    $menu3 = "menu-item3";
+
+    echo "<li>$menu</li><li>$menu2</li><li>$menu3</li>";
+    ?>
+
+    </ul>
+</body>
+</html>
