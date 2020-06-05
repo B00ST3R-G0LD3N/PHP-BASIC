50accabaa063570f802f62c1b112977289d081f3
Sofyan Ahalli <sofyan.ahalli@student.rocva.nl>
2020-06-05 04:41:51 +0200

    taak 6

---
+++ 5-Conditional_Statements/taak06/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$var1 = 10;
+$var2 = 11;
+if($var1 < 10 && $var1 > 5) {
+    echo "het getal zit tussen de 10 en de 5!";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!";
+}
+
+if($var1 < 20 && $var1 > 5if($var1 < 10 && $var1 > 5) {
+    echo "het getal zit tussen de 10 en de 5!";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!";
+}
+
+) {
+    echo "het getal zit tussen de 10 en de 5!";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!";
+}
+
+if($var1 < 10 && $var1 > 5) {
+    echo "het getal zit tussen de 10 en de 5!";
+} else {
+    echo "het getal is groter dan 10 of kleiner dan 5!";
+}
+
+echo $var1;
+echo $var2;
+
+?>    
+</body>
+</html>
