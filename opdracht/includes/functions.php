f1cd2280c8638b8adadf4528accf01018ced7333
Sofyan Ahalli <sofyan.ahalli@student.rocva.nl>
2020-06-22 14:26:25 +0200

    Eind opdracht php basic

--- opdracht/includes/functions.php
+++ opdracht/includes/functions.php
@@ -1,9 +1,12 @@
 <?php
 
+$productenLijst = array ("fanta", "pinda's", "cake", "bananen", "appels", "milkshake", "schoonmaak doek", "onderbroeken", "flesje water", "melk");
+$productenPrijs = array ( 1,75, 1,46, 2.43, 1,21, 1,60, 4,50, 1.40, 21,50, 2,17, 1,45);
+
 function getInput()
 {
     $input = $_GET['input'] ?? 'vul een waarde in hierboven en klik op [check]';
-    echo $input;
+    echo var_dump($input);
 }
 
 
@@ -22,7 +25,50 @@ function getAmount()
 function getAge()
 {
     $leeftijd = $_GET['leeftijd'] ?? 'vul een leeftijd in!';
+    switch($leeftijd) 
+    { 
+        case 19:  
+        break;
+        case 18:       
+        break;
+        case 17:
+        break;
+        case 16:
+        break;
+        default:     
+    }
     echo $leeftijd;
 }
+
+function getDiscount()
+{
+    $leeftijd = $_GET['leeftijd'] ?? 'vul een leeftijd in!';
+    switch($leeftijd) 
+    {
+        case 0.19:
+        break;
+        case 18:       
+        break;
+        case 17:
+        break;
+        case 16:
+        break;
+        default:     
+    }
+    echo $leeftijd;
+}
+
+function getPrijs()
+{
+    $productenLijst = array ("fanta", "pinda's", "cake", "bananen", "appels", "milkshake", "schoonmaak doek", "onderbroeken", "flesje water", "melk");
+    $productenPrijs = array ( 1,75, 1,46, 2.43, 1,21, 1,60, 4,50, 1.40, 21,50, 2,17, 1,45);
+
+    $productnummer = $_GET['productnummer'] ?? 'vul een productnummer in!';
+    $aantalProd = $_GET['aantal'] ?? 'vul een aantal in!';
+    
+    echo $aantalProd * $productenPrijs[$productnummer];
+}
+
+
 
 ?>
--- opdracht/includes/menu.php
+++ opdracht/includes/menu.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <ul>
-    <?php
-    $menu = "menu-item1";
-    $menu2 = "menu-item2";
-    $menu3 = "menu-item3";
-
-    echo "<li>$menu</li><li>$menu2</li><li>$menu3</li>";
-    ?>
-
-    </ul>
-</body>
-</html>
+<ul>
+<li><a href='index.php'>home</a></li>
+<li><a href='producten.php'>producten</a></li>
+</ul>
